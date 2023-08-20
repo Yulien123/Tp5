745fe6906f785cff2ee7f2ad1544c51ab3548b5b
Julio Flores <juliofloresth@gmail.com>
2023-08-20 18:59:59 -0300

    Initial commit

---
+++ src/TestMain/Tp5.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package TestMain;
+
+import entity.ListaProductos;
+
+/**
+ *
+ * @author julan
+ */
+public class Tp5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new ListaProductos().setVisible(true);
+       
+    }
+    
+}
