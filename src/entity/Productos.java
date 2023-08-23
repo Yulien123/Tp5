f0fe7f73a148075215d12824272dc45610eb7996
Julio Flores <juliofloresth@gmail.com>
2023-08-23 18:14:32 -0300

    add Productos

---
+++ src/entity/Productos.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author julan
+ */
+public class Productos {
+    private String nombre;
+    private String categoria;
+    private double precio;
+
+    public Productos(String nombre, String categoria, double precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Productos{" + "nombre=" + nombre + ", categoria=" + categoria + ", precio=" + precio + '}';
+    }
+    
+    
+}
